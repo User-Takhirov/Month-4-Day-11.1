884056e1a54c760213b4092c4b58488c6ed863de
User-Takhirov <tohirovfayruz@gmail.com>
2024-09-13 18:41:02 +0500

    js day 11.1

---
+++ src/data/main.js
@@ -0,0 +1,111 @@
+const url = "http://127.0.0.1:5500/src/img";
+
+export const Banners = [
+  {
+    headerLeftimg: `${url}/bulb.svg`,
+    miniText: "Best Furniture For Your Castle....",
+    title: `New Furniture Collection
+      Trends in 2020`,
+    headerText: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing
+      in phasellus non in justo.`,
+    headerBtn: "ShopNow",
+    HeaderImg: `${url}/caoch.svg`,
+  },
+];
+
+export const FeaturedProducts = [
+  {
+    featuredTitle: "Featured Products",
+    featuredImg: `${url}/chair.svg`,
+    featuredInnerTitle: "Cantilever chair",
+    featuredDes: `${url}/featured_des.svg`,
+    featuredText: "Code - Y523201",
+    featuredPrice: "$42.00",
+  },
+  {
+    featuredTitle: "Featured Products",
+    featuredImg: `${url}/chair.svg`,
+    featuredInnerTitle: "Cantilever chair",
+    featuredDes: `${url}/featured_des.svg`,
+    featuredText: "Code - Y523201",
+    featuredPrice: "$42.00",
+  },
+  {
+    featuredTitle: "Featured Products",
+    featuredImg: `${url}/chair.svg`,
+    featuredInnerTitle: "Cantilever chair",
+    featuredDes: `${url}/featured_des.svg`,
+    featuredText: "Code - Y523201",
+    featuredPrice: "$42.00",
+  },
+  {
+    featuredTitle: "Featured Products",
+    featuredImg: `${url}/chair.svg`,
+    featuredInnerTitle: "Cantilever chair",
+    featuredDes: `${url}/featured_des.svg`,
+    featuredText: "Code - Y523201",
+    featuredPrice: "$42.00",
+  },
+];
+
+export const LatestProducts = [
+  {
+    img: `${url}/latestchair.png`,
+    text: "Comfort Handy Craft",
+    price: "$42.00",
+    previousPrice: "$65.00",
+  },
+  {
+    img: `${url}/latestchair.png`,
+    text: "Comfort Handy Craft",
+    price: "$42.00",
+    previousPrice: "$65.00",
+  },
+  {
+    img: `${url}/latestchair.png`,
+    text: "Comfort Handy Craft",
+    price: "$42.00",
+    previousPrice: "$65.00",
+  },
+  {
+    img: `${url}/latestchair.png`,
+    text: "Comfort Handy Craft",
+    price: "$42.00",
+    previousPrice: "$65.00",
+  },
+  {
+    img: `${url}/latestchair.png`,
+    text: "Comfort Handy Craft",
+    price: "$42.00",
+    previousPrice: "$65.00",
+  },
+  {
+    img: `${url}/latestchair.png`,
+    text: "Comfort Handy Craft",
+    price: "$42.00",
+    previousPrice: "$65.00",
+  },
+];
+
+export const Offer = [
+  {
+    img: `${url}/offer.svg`,
+    title: "24/7 Support",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa purus gravida.",
+  },
+  {
+    img: `${url}/Offer2.svg`,
+    title: "24/7 Support",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa purus gravida.",
+  },
+  {
+    img: `${url}/offer.svg`,
+    title: "24/7 Support",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa purus gravida.",
+  },
+  {
+    img: `${url}/offer.svg`,
+    title: "24/7 Support",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa purus gravida.",
+  },
+];
